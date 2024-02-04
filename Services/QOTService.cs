21839d8025f7c86f02d068c68be226069577e562
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-02-04 14:53:01 +0100

    add async db

--- Services/QOTService.cs
+++ Services/QOTService.cs
@@ -1,34 +1,35 @@
 ﻿using DarkBot.Models;
 using LiteDB;
+using LiteDB.Async;
 
 namespace DarkBot.Services;
 
 public class QOTService
 {
-    public QOTModel GetQOTModel()
+    public async Task<QOTModel> GetQOTModel()
     {
-        using LiteDatabase db = new LiteDatabase("Data.db");
+        using var db = new LiteDatabaseAsync("Data.db");
         // retrieve the entire QOT and its documents (each document is like one json object)
         var qotCollection = db.GetCollection<QOTModel>("QOT");
             
         // select the first document from the array
-        var qot = qotCollection.FindOne(Query.All());
+        var qot = await qotCollection.FindOneAsync(Query.All());
             
         if (qot is null)
         {
             var newQOTModel = new QOTModel(1, DateTime.Today.AddDays(-1));
             // if for some reason the Data.db would have been deleted or corrupted, then new document will be created
-            qotCollection.Insert(newQOTModel);
+            await qotCollection.InsertAsync(newQOTModel);
             return newQOTModel;
         }
 
         return qot;
     }
     
-    public void UpdateQOTModel(QOTModel qot)
+    public async Task UpdateQOTModel(QOTModel qot)
     {
-        using LiteDatabase db = new LiteDatabase(@"Data.db");
+        using var db = new LiteDatabaseAsync(@"Data.db");
         var qotCollection = db.GetCollection<QOTModel>("QOT");
-        qotCollection.Update(qot);
+        await qotCollection.UpdateAsync(qot);
     }
 }
