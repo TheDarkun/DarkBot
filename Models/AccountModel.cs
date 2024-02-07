5a940f88c8d0e429f305169ab78987a193cd7a83
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-02-07 17:07:03 +0100

    add authorization and authentication

---
+++ Models/AccountModel.cs
@@ -0,0 +1,12 @@
+﻿using LiteDB;
+
+namespace DarkBot.Models;
+
+public class AccountModel(string access_token, string refresh_token, int expires_in)
+{
+    public string Id { get; set; } = ObjectId.NewObjectId().ToString();
+    
+    public string AccessToken { get; set; } = access_token;
+    public string RefreshToken { get; set; } = refresh_token;
+    public DateTime ExpiresIn { get; set; } = DateTime.Now.AddSeconds(expires_in);
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DarkBot.Models;
+
+public class UserModel(string id, string username, string global_name, string avatar)
+{
+    public string Id { get; set; } = id;
+    public string Username { get; set; } = username;
+    public string? GlobalName { get; set; } = global_name;
+    public string? Avatar { get; set; } = avatar;
+}
