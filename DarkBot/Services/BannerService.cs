0cd1db2602ec4e8927043323337bf33ad3cb5568
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-02-25 21:21:06 +0100

    add discord and web image upload

--- DarkBot/Services/BannerService.cs
+++ DarkBot/Services/BannerService.cs
@@ -1,6 +1,30 @@
-﻿namespace DarkBot.Services;
+﻿using Microsoft.AspNetCore.Components.Forms;
 
-public class BannerService
+namespace DarkBot.Services;
+
+public class BannerService(IWebHostEnvironment env, HttpClient client)
 {
-    
+    public async Task SaveFile(IBrowserFile file)
+    {
+        // TODO: GUID NAME
+        var path = Path.Combine(env.WebRootPath, "images", "balls");
+        await using FileStream stream = new(path, FileMode.Create);
+        
+        var s = file.OpenReadStream(1024 * 1024 * 4);
+        await s.CopyToAsync(stream);
+    }
+
+    public async Task UploadFile(string url, bool isPng)
+    {
+        var result = await client.GetAsync(url);
+        if (result.IsSuccessStatusCode)
+        {
+            // TODO: GUID NAME
+            var path = Path.Combine(env.WebRootPath, "images", $"balls{(isPng ? ".png" : ".jpg")}");
+            await using FileStream stream = new(path, FileMode.Create);
+        
+            var s = await result.Content.ReadAsStreamAsync();
+            await s.CopyToAsync(stream); 
+        }
+    }
 }
